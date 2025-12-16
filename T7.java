8b4536b6a6ea195ee0f4daec1acb9a341ac87c7e
Kausab Badgujar <kausabbadgujar01@gmail.com>
2025-12-16 17:06:39 +0530

    commit

---
+++ T7.java
@@ -0,0 +1,19 @@
+import java.util.TreeMap;
+
+public class T7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap<String, String> tm=new TreeMap<String, String>();
+	    
+	    tm.put("C1", "Red");
+        tm.put("C2", "Green");
+        tm.put("C3", "Black");
+        tm.put("C4", "White");
+	    System.out.println(tm);
+	    
+	    
+	    System.out.println("new map: "+tm);
+	}
+
+}
